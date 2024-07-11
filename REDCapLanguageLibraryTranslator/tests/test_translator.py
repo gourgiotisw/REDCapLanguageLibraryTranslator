c9e14e003e223b45a26d275d74d7c37884b4c9b2
William Gourgiotis <64996193+gourgiotisw@users.noreply.github.com>
2024-07-10 20:00:51 -0400

    Add files via upload

---
+++ REDCapLanguageLibraryTranslator/tests/test_translator.py
@@ -0,0 +1,11 @@
+import unittest
+from redcapllt import translator
+
+class TestTranslator(unittest.TestCase):
+
+    def test_translation(self):
+        # Add your test cases
+        pass
+
+if __name__ == '__main__':
+        unittest.main()

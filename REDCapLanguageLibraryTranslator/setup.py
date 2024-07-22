2d279c1807346c40b1656252629968dfc007425f
William Gourgiotis <wgourgiotis@gmail.com>
2024-07-22 15:36:05 -0400

    update setup.py

--- REDCapLanguageLibraryTranslator/setup.py
+++ REDCapLanguageLibraryTranslator/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="REDCapLLT",
-    version="0.2.0",
+    version="2.0.0",
     author="William Gourgiotis",
     author_email="william.gourgiotis@umassmed.edu",
     description="A package to translate REDCap language libraries",
